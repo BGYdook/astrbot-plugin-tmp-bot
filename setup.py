7b83dc21544f16ec73e775ee779dc4c7061c585b
BGYdook <3317732779@qq.com>
2025-10-04 14:33:20 +0800

    安装了python

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+AstrBot-plugin-tmp-bot
+欧卡2TMP查询插件 - Python辅助脚本
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="astrbot-plugin-tmp-bot",
+    version="1.0.0",
+    description="欧卡2TMP查询插件",
+    author="BGYdook",
+    author_email="",
+    url="https://github.com/BGYdook/AstrBot-plugin-tmp-bot",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
